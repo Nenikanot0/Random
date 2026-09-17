55777c505b9de00346c6579ec83406ca773a20cb
Nenikanot0 <meherneharika503@gmail.com>
2026-09-18 00:44:49 +0530

    added login user

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -37,3 +37,41 @@ export const registerUser = async(req,res) => {
         return res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : error.message});
     }
 }
+
+export const loginUser = async(req,res) => {
+    try{
+        const { email,password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({ message: "Fill all details" });
+        }
+
+        const user = await User.findOne({ email:email }).select("+password");
+
+        if(!user){
+            return res.status(401).json({ message:"Email or password invalid" });
+        }
+
+        const isMatch = await user.comparePassword(password);
+
+        if(!isMatch){
+            return res.status(401).json({ message:"Email or Password is invalid." })
+        }
+        
+        const token = jwt.sign({userId : user._id} ,process.env.JWT_SECRET, { expiresIn:"3d" });
+
+        res.cookie("token",token);
+
+        res.status(200).json({
+            user:{
+                _id:user._id,
+                username:user.username,
+                email:user.email,
+            },
+            token
+        });
+    }catch(error){
+        console.log(error.message);
+        return res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : error.message});
+    }
+}
